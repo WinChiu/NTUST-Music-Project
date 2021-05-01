f08dadfc7544c3f8efd4d23e0d54d03329c3f201
Win Chiu <win881206@gmail.com>
2021-05-01 17:14:18 +0800

    some mainpage animate

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let arrowClick = false;
+
 $("#logoRed #menu img.arrow").click(function () {
   console.log(arrowClick);
   if (!arrowClick) {
@@ -9,3 +10,30 @@ $("#logoRed #menu img.arrow").click(function () {
   }
   arrowClick = !arrowClick;
 });
+
+// $(window).on("wheel", function () {
+//   $(".home .bigBackground img.background").css("top", "-92vh");
+//   $(".home .bigBackground .introContainer").css("top", "0vh");
+// });
+
+document.addEventListener("swiped-up", function () {
+  $(".home .bigBackground img.background").css("top", "-92vh");
+  $(".home .bigBackground .introContainer").css("top", "0vh");
+  $("#logoRed").css("top", "0vh");
+});
+
+document.addEventListener("swiped-down", function () {
+  $(".home .bigBackground img.background").css("top", "0vh");
+  $(".home .bigBackground .introContainer").css("top", "100vh");
+});
+
+$(".arrowIcon").click(function () {
+  $(".home .bigBackground img.background").css("top", "-100vh");
+  $(".home .bigBackground .introContainer").css({ top: "20vh", opacity: "0" });
+  $(".tutorial").fadeIn();
+  $(".tutorial").css("display", "flex");
+});
+
+// $(".arrowIcon").click(function () {
+//   $("img.logo").attr("src", "./assets/img/brandRed.svg");
+// });
