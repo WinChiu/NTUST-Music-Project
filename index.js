137871ee985f244dcc353f14488f78c4cd7707c8
Win Chiu <win881206@gmail.com>
2021-05-01 16:18:46 +0800

    nav animation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+let arrowClick = false;
+$("#logoRed #menu img.arrow").click(function () {
+  console.log(arrowClick);
+  if (!arrowClick) {
+    $("#logoRed #menu .toggle").fadeIn();
+    $("#logoRed #menu .toggle").css("display", "flex");
+  } else {
+    $("#logoRed #menu .toggle").fadeOut();
+  }
+  arrowClick = !arrowClick;
+});
